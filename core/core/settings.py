67ec56e1547c043889136bb2995322cf52240ef7
Esmael <82362802+esmael1997@users.noreply.github.com>
2026-02-20 17:45:02 +0330

    update settings.py for PostgreSQL database configuration

--- core/core/settings.py
+++ core/core/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
